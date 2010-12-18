333fd1fb178858aaa9b624c37ad4b9355e29bc79
Lenny Marks <lenny@lenny.ridge.aps.org>
2010-12-17 22:11:51 -0500

    WIP

---
+++ lib/log4jruby/log4j_args.rb
@@ -0,0 +1,45 @@
+module Log4jruby
+  class Log4jArgs
+    class << self
+      def convert(object = nil, exception = nil)
+
+
+        msg = ''
+
+        if exception
+          msg << object.to_s
+        else
+          exception = block_given? ? yield : object
+        end
+
+        if exception.is_a?(NativeException)
+          append_ruby_error(msg, exception)
+          exception = exception.cause
+        elsif exception.is_a?(::Exception)
+          append_ruby_error(msg, exception)
+          exception = nil
+        elsif !exception.is_a?(java.lang.Throwable)
+          msg << exception.to_s
+          exception = nil
+        end
+
+        [msg, exception]
+
+      end
+
+      private
+
+      def append_ruby_error(msg, error)
+        append(msg, "#{error}\n  " + error.backtrace.join("\n  "))
+      end
+
+      def append(msg, s)
+        if msg.empty?
+          msg << s
+        else
+          msg << "\n#{s}"
+        end
+      end
+    end
+  end
+end
--- spec/log4jruby/logger_spec.rb
+++ spec/log4jruby/logger_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require 'spec_helper'
 
 require 'log4jruby'
 
@@ -61,6 +61,10 @@ module Log4jruby
       
       it "should respond to :level" do
         subject.respond_to?(:level).should == true
+      end
+      
+      it "should respond to :flush" do
+        subject.respond_to?(:flush).should == true
       end
     end
     
